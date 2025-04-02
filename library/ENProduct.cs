6f58b1dc51333ca75abc33394bf1c4183215f1dc
Francisco Navarro Martinez <chupirrapucha@gmail.com>
2025-04-02 12:18:00 +0200

    74443290H Añadidos los metodos publicos de los atributos y acabados los constructores

--- library/ENProduct.cs
+++ library/ENProduct.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Xml.Linq;
 
 namespace library
 {
@@ -14,7 +15,52 @@ namespace library
         private int category { get; set; }
         private float price { get; set; }
         private DateTime creationDate { get; set; }
-        
+
+        public string Code
+        {
+            get { return code; }
+            set { code = value; }
+
+        }
+        public string Name
+        {
+            get { return name; }
+            set { name = value; }
+        }
+        public int Amount
+        {
+            get { return amount; }
+            set { amount = value; }
+        }
+        public int Category
+        {
+            get { return category; }
+            set { category = value; }
+        }
+        public float Price
+        {
+            get { return price; }
+            set { price = value; }
+        }
+        public DateTime CreationDate
+        {
+            get { return creationDate; }
+            set { creationDate = value; }
+        }
+        public ENProduct()
+        {
+
+        }
+        public ENProduct(string code, string name, int amount, float price, int category, DateTime creationDate)
+        {
+            Code = code;
+            Name = name;
+            Amount = amount;
+            Category = category;
+            Price = price;
+            CreationDate = creationDate;
+
+        }
 
     }
 }
